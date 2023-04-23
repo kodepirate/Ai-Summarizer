623b880708a366e7a7563a822527323efc9f1625
kodepirate <aniket.das9062@gmail.com>
2023-04-23 18:12:36 +0530

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,29 @@
+import {logo} from '../assets';
+
+const Hero = () => {
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+        <nav className="flex justify-between items-center w-full mb-10 pt-3">
+            <img src={logo} alt="summarizer_logo" className='w-28 object-contain' />
+
+            <button type="button"
+            onClick={() => window.open('https://github.com/kodepirate')}
+            className = "black_btn"
+            >
+                <span className="text-white">View on Github</span>
+            </button>
+        </nav>
+
+        <h1 className="head_text">
+        Summarizer <br className='max-md:hidden' /> 
+        <span className="orange_gradient">OPEN-AI  </span>
+        </h1>
+        <h2 className="desc">
+        Summarizer is a web app that uses Open-AI GPT-3 to summarize  any text.
+
+        </h2>
+    </header>
+    )
+}
+
+export default Hero
